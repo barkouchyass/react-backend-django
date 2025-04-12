e2a4476f41b740994eed6d5017ae0f81b0097966
barkouchyass <barkouchyass@gmail.com>
2025-04-12 21:48:16 +0100

    Link backend to React

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,17 @@
 from customers.models import Customer
 from django.http import JsonResponse
 from customers.serializers import customerSerializer
-def customers(request): 
-   data = Customer.objects.all()
-   serializer=customerSerializer(data, many=True)
-   return JsonResponse(serializer.data)
 
+def customers(request):
+   data = Customer.objects.all()
+   serializer = customerSerializer(data, many=True)
+   # Modifier la structure des données ici
+   response_data = {"customers": serializer.data}
+   return JsonResponse(response_data)
+
+def customer(request, id):
+   data = Customer.objects.get(pk=id)
+   serializer = customerSerializer(data)
+   # Modifier la structure des données ici
+   response_data = {"customer": serializer.data}
+   return JsonResponse(response_data)
